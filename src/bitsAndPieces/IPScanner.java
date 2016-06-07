a9184d75c862639dd1989c06a890e62537e63ce5
Catalin Podariu <catalin.podariu@gmail.com>
2016-06-08 00:36:35 +0200

    clean code refactoring

--- src/bitsAndPieces/IPScanner.java
+++ src/bitsAndPieces/IPScanner.java
@@ -7,151 +7,137 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * Simple IP Scanner, just searches 192.168.[your mask].*
+ * Simple IP Scanner, just for 192.168.[your mask].*
  *
  * @author catalin.podariu@gmail.com
  */
 public class IPScanner {
 
-    private IPScanner() {
-    }
+	private String[] hosts;
+	private ArrayList<String> aliveServers;
+	private ArrayList<Future<String>> results;
 
-    public static IPScanner valueOf() {
-        return new IPScanner();
-    }
+	private final int INTERRUPTED_ACTION = -3;
+	private final int LARGE_NUMBER_INPUT = -2;
+	private final int NEGATIVE_NUMBER_INPUT = -1;
 
-    public String[] backupScan(int pingTimeout) {
-        String temp = "";
-        String[] localhost = null;
-        try {
-            // get local IP address 
-            temp = InetAddress.getLocalHost().toString();
-            // split the IP address into individual octets
-            localhost = temp.substring(temp.indexOf("/") + 1).split("[^\\d]");
-        } catch (UnknownHostException ex) {
-            System.err.println(ex);
-        }
-        // the 3rd octet of the IP address
-        int status = scanner(Integer.valueOf(localhost[2]), pingTimeout, 50);
-        // if there thread is not interrupted 
-        if (status != -3) {
-            if (aliveServers.isEmpty()) {
-                hosts = new String[1];
-                hosts[0] = "No alive servers..";
-            } else {
-                hosts = new String[aliveServers.size()];
-                for (String aliveIPAddress : aliveServers) {
-                    hosts[0] = temp.substring(0, temp.indexOf("/"));
-                    hosts[1] = aliveIPAddress;
-                    hosts[2] = "n/a";
-                }
-            }
-        } else {
-            hosts[0] = "Scan error..";
-        }
-        return hosts;
-    }
+	private final int DEFAULT_THREADS_NUMBER = 50;
+	private final int MAX_THREADS_NUMBER = 64;
 
-    /**
-     * Simple IP Scanner, just searches 192.168.[your mask].*
-     *
-     * @param mask gets that from local host IP address
-     * @param timeout time-out to wait until connect (in milliseconds).
-     * @param nThreads number of threads to run simultaneously. If there are 50
-     * threads, it will take ~10 seconds to scan 250 ip addresses
-     * @return 0 for OK; -1 & -2 for wrong numbers received (large or negative)
-     * and -3 for InterruptedException.
-     */
-    public int scanner(int mask, int timeout, int nThreads) {
-        if (mask > 254) {
-            // can't have large numbers.
-            return -1;
-        }
-        if (mask < 0) {
-            // can't have negative numbers.
-            return -2;
-        }
-        aliveServers = new ArrayList();
-        // launch a maximum of 64 threads at once
-        if (nThreads >= 64) {
-            nThreads = 64;
-        }
-        exec = Executors.newFixedThreadPool(nThreads);
-        results = new ArrayList();
-        Future status;
-        // create an ArrayList of Future objects
-        for (int i = 0; i < 255; i++) {
-            status = exec.submit(
-                    new ScannerThread("192.168." + mask + "." + i, timeout));
-            results.add(status);
-        }
-        // cleanup
-        exec.shutdown();
-        // query every object in the results[] for status
-        for (Future item : results) {
-            try {
-                if (item.get().toString().endsWith("alive")) {
-                    /*
-                     if the item is alive, add to aliveServers strip 
-                     the " is alive" from the end of String
-                     */
-                    aliveServers.add(item.get().toString().substring(
-                            0,
-                            item.get().toString().indexOf(" ")
-                    ));
-                }
-            } catch (InterruptedException | ExecutionException ex) {
-                System.err.println(ex);
-                return -3;
-            }
-        }
-        return 0;
-    }
+	private IPScanner() {
+	}
 
-    class ScannerThread implements Callable {
+	public static IPScanner valueOf() {
+		return new IPScanner();
+	}
 
-        private String ipAsString;
-        private int timeout;
+	public String[] init(int pingTimeout) {
+		String temp = "";
+		String[] localhost = null;
+		try {
+			temp = InetAddress.getLocalHost().toString();
+			// split the IP address into individual octets
+			localhost = temp.substring(temp.indexOf("/") + 1).split("[^\\d]");
+		} catch (UnknownHostException ex) {
+			System.err.println(ex);
+		}
+		// the 3rd octet
+		int status = scan(Integer.valueOf(localhost[2]), pingTimeout, DEFAULT_THREADS_NUMBER);
+		if (status != INTERRUPTED_ACTION) {
+			if (aliveServers.isEmpty()) {
+				hosts = new String[1];
+				hosts[0] = "No alive servers..";
+			} else {
+				hosts = new String[aliveServers.size()];
+				for (String ipNumber : aliveServers) {
+					hosts[0] = temp.substring(0, temp.indexOf("/"));
+					hosts[1] = ipNumber;
+					hosts[2] = "n/a";
+				}
+			}
+		} else {
+			hosts[0] = "Scan error..";
+		}
+		return hosts;
+	}
 
-        /**
-         * Thread that will test one IP address.
-         *
-         * @param ipAsString
-         * @param timeout
-         */
-        public ScannerThread(String ipAsString, int timeout) {
-            this.ipAsString = ipAsString;
-            this.timeout = timeout;
-        }
+	public int scan(int ipMask, int timeout, int nThreads) {
+		if (ipMask > 254) {
+			return NEGATIVE_NUMBER_INPUT;
+		}
+		if (ipMask < 0) {
+			return LARGE_NUMBER_INPUT;
+		}
+		aliveServers = new ArrayList<>();
 
-        @Override
-        public String call() {
-            try {
-                InetAddress inet = InetAddress.getByName(ipAsString);
-//            System.out.println("Ping request to IP: " + ipAsString);
-                if (inet.isReachable(timeout)) {
-                    return ipAsString + " is alive";
-                } else {
-                    return ipAsString + " is dead";
-                }
-            } catch (SocketTimeoutException ex) {
-                System.err.println("Timed out: " + ipAsString);
-                return "Timed out: " + ipAsString;
-            } catch (IOException ex) {
-                System.out.println(ex);
-                return ex.toString();
-            }
-        }
-    }
+		if (nThreads > MAX_THREADS_NUMBER) {
+			nThreads = MAX_THREADS_NUMBER;
+		}
+		try (ACExecutorService exec = ACExecutorService.newFixedPool(nThreads)) {
+			results = new ArrayList<>();
+			Future<String> status;
+			// create an ArrayList of Future objects
+			for (int i = 0; i < 255; i++) {
+				status = exec.submit(new Scanner("192.168." + ipMask + "." + i, timeout));
+				results.add(status);
+			}
+		}
+		return queryResultsForStatus();
+	}
 
-    private String[] hosts;
-    private ArrayList<String> aliveServers;
-    private ArrayList<Future> results;
-    private ExecutorService exec;
+	private int queryResultsForStatus() {
+		for (Future<String> item : results) {
+			try {
+				if (item.get().toString().endsWith("alive")) {
+					/*
+					 * if the item is alive, add to aliveServers strip the
+					 * " is alive" from the end of String
+					 */
+					aliveServers.add(item.get().toString().substring(0, item.get().toString().indexOf(" ")));
+				}
+			} catch (InterruptedException | ExecutionException ex) {
+				System.err.println(ex);
+				return INTERRUPTED_ACTION;
+			}
+		}
+		return 0;
+	}
 
+	class Scanner implements Callable<String> {
+
+		private String ipAsString;
+		private int timeout;
+
+		public Scanner(String ipAsString, int timeout) {
+			this.ipAsString = ipAsString;
+			this.timeout = timeout;
+		}
+
+		@Override
+		public String call() {
+			try {
+				return returnAliveOrDeadStatus();
+			} catch (SocketTimeoutException ex) {
+				System.err.println("Timed out: " + ipAsString);
+				return "Timed out: " + ipAsString;
+			} catch (IOException ex) {
+				System.out.println(ex);
+				return ex.toString();
+			}
+		}
+
+		private String returnAliveOrDeadStatus() throws UnknownHostException, IOException {
+
+			InetAddress inet = InetAddress.getByName(ipAsString);
+			// System.out.println("Ping request to IP: " + ipAsString);
+			if (inet.isReachable(timeout)) {
+				return ipAsString + " is alive";
+			} else {
+				return ipAsString + " is dead";
+			}
+		}
+	}
 }
